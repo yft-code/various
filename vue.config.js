489224574cd93bd99c29b26278671a4a40bb6eea
LAPTOP-AG4T4L0N\yft123 <2997978921@qq.com>
2023-02-06 14:03:56 +0800

    vue.config.js官网配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+//vue.config.js的官网配置
+//https://cli.vuejs.org/zh/config/#vue-config-js
 const path=require('path')//nodejs核心模块，专门用来处理路径问题
 // const resolve = (dir) => path.join(__dirname, dir);
 module.exports={
